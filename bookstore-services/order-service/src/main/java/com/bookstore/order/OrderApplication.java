91efbe074a2a931897ec82a1dd8b82c7edba91ee
GavinKim <dreamworld612@gmail.com>
2019-02-07 23:59:25 +0900

    modify services

--- bookstore-services/order-service/src/main/java/com/bookstore/order/OrderApplication.java
+++ bookstore-services/order-service/src/main/java/com/bookstore/order/OrderApplication.java
@@ -2,7 +2,15 @@ package com.bookstore.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderApplication {
 
@@ -11,4 +19,3 @@ public class OrderApplication {
     }
 
 }
-
